e2cbb58a2cda94f72f9b5b4157292c34534d42ee
Laura Côté <lauracote871@gmail.com>
2020-12-01 21:48:48 -0500

    puzzle 1

---
+++ puzzle-1.py
@@ -0,0 +1,31 @@
+def main():
+    print(computeEntries(getEntries(), 2020))
+
+def getEntries():
+    with open('data/data-1.txt') as file:
+        return [int(line.rstrip()) for line in file]
+
+# complexity : nlogn
+def computeEntries(entries, total):
+    entries.sort()
+    filter(lambda entry: entry < total, entries)
+    for entry in entries:
+        if entry >= total:
+            return -1
+        found = findEntry(entries, 0, len(entries) - 1, total - entry)
+        if found != -1:
+            return entries[found] * entry
+    return -1
+
+def findEntry(entries, lowerbound, upperbound, entry): 
+    if upperbound >= lowerbound: 
+        middle = (upperbound + lowerbound) // 2
+        if entries[middle] == entry: 
+            return middle 
+        if entries[middle] > entry: 
+            return findEntry(entries, lowerbound, middle - 1, entry) 
+    
+        return findEntry(entries, middle + 1, upperbound, entry)
+    return -1
+
+main()

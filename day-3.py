de6008479b7427d3b285600a6c818ca259365973
Laura Côté <lauracote871@gmail.com>
2020-12-03 19:47:33 -0500

    day 3 part 2

--- day-3.py
+++ day-3.py
@@ -5,9 +5,14 @@ def get_entries():
     with open('data/day-3.txt') as file:
         return [(line.rstrip()) for line in file]
 
-def number_of_trees(entries):
+def number_of_trees(entries, right, down):
     max_x = len(entries[0])
-    return len([1 for index, line in enumerate(entries) if line[((index * 3 + 1) % max_x) - 1] == '#'])
+    return len([1 for index, line in enumerate(entries) if index % down == 0 and line[(((index // down) * right + 1) % max_x) - 1] == '#'])
+
+def total_trees(entries):
+    return number_of_trees(entries, 1, 1) * number_of_trees(entries, 3, 1) * number_of_trees(entries, 5, 1) * number_of_trees(entries, 7, 1) * number_of_trees(entries, 1, 2)
 
 if __name__ == "__main__":
-    print(number_of_trees(get_entries()))
+    entries = get_entries()
+    print(number_of_trees(entries, 3, 1))
+    print(total_trees(entries))

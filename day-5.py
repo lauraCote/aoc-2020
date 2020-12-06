a4c96fd5a3d25be423b521979998dd03c09e2b83
Laura Côté <lauracote871@gmail.com>
2020-12-06 11:24:03 -0500

    concat two next functions

--- day-5.py
+++ day-5.py
@@ -8,27 +8,20 @@ def get_seats():
     with open('data/day-5.txt') as file:
         return [(line.rstrip()) for line in file]
 
-def next_row(row_range, direction):
-    middle = (row_range[1] - row_range[0]) // 2
-    if direction == 'F':
-        return (row_range[0], row_range[1] - middle - 1)
-    if direction == 'B':
-        return (1 + row_range[0] + middle, row_range[1])
-
-def next_column(column_range, direction):
-    middle = (column_range[1] - column_range[0]) // 2
-    if direction == 'L':
-        return (column_range[0], column_range[1] - middle - 1)
-    if direction == 'R':
-        return (1 + column_range[0] + middle, column_range[1])
+def next(range, direction):
+    middle = (range[1] - range[0]) // 2
+    if direction == 'F' or direction == 'L':
+        return (range[0], range[1] - middle - 1)
+    if direction == 'B' or direction == 'R':
+        return (1 + range[0] + middle, range[1])
 
 def get_seat_id(seat):
     data = re.match(r'([FB]{1,7})([LR]{3})', seat).groups()
     row_range = (0, ROWS - 1)
-    row = functools.reduce(next_row, data[0], row_range)[0]
+    row = functools.reduce(next, data[0], row_range)[0]
     
     column_range = (0, SEATS_PER_ROW - 1)
-    column = functools.reduce(next_column, data[1], column_range)[0]
+    column = functools.reduce(next, data[1], column_range)[0]
 
     return row * SEATS_PER_ROW + column
 

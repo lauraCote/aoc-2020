670e45f832703ce93f0eea1c72020cd8cd6d0944
Laura Côté <lauracote871@gmail.com>
2020-12-08 21:32:48 -0500

    change passwords to passport

--- day-4.py
+++ day-4.py
@@ -1,42 +1,42 @@
 import re
 
-def get_passwords():
+def get_passports():
     data = open('data/day-4.txt').read().split("\n\n")
-    passwords = list(map(lambda password: password.replace('\n', ' ').split(' '), data))
-    return list(dict(map(lambda info: info.split(':'), password)) for password in passwords)  
+    passports = list(map(lambda passport: passport.replace('\n', ' ').split(' '), data))
+    return list(dict(map(lambda info: info.split(':'), passport)) for passport in passports)  
 
-def is_valid(password):
-    if not contains_all_fields(password):
+def is_valid(passport):
+    if not contains_all_fields(passport):
         return False
 
     height_valid = False
-    height = re.split(r'(\d+)', password['hgt'])
+    height = re.split(r'(\d+)', passport['hgt'])
     if height[2] == 'cm':
         height_valid = 150 <= int(height[1]) <= 193
     if height[2] == 'in':
         height_valid = 59 <= int(height[1]) <= 76 
 
-    hair_valid = len(password['hcl']) == 7 and password['hcl'][:-6] == '#' and re.match(r'[\w-]*$', password['hcl'][-6:]) != None
+    hair_valid = len(passport['hcl']) == 7 and passport['hcl'][:-6] == '#' and re.match(r'[\w-]*$', passport['hcl'][-6:]) != None
         
     return all([
         hair_valid,
         height_valid,
-        1920 <= int(password['byr']) <= 2002,
-        2010 <= int(password['iyr']) <= 2020,
-        2020 <= int(password['eyr']) <= 2030,
-        re.match(r'^\d{9}$', password['pid']),
-        password['ecl'] in { 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'} 
+        1920 <= int(passport['byr']) <= 2002,
+        2010 <= int(passport['iyr']) <= 2020,
+        2020 <= int(passport['eyr']) <= 2030,
+        re.match(r'^\d{9}$', passport['pid']),
+        passport['ecl'] in { 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'} 
     ])
 
-def contains_all_fields(password):
+def contains_all_fields(passport):
     requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    return all(map(lambda info: info in password, requiredFields))
+    return all(map(lambda info: info in passport, requiredFields))
 
-def get_valid_passwords_count(passwords, validator):
-    return len([1 for password in passwords if validator(password)])
+def get_valid_passports_count(passports, validator):
+    return len([1 for passport in passports if validator(passport)])
 
 
 if __name__ == "__main__":
-    passwords = get_passwords()
-    print(get_valid_passwords_count(passwords, contains_all_fields))
-    print(get_valid_passwords_count(passwords, is_valid))
+    passports = get_passports()
+    print(get_valid_passports_count(passports, contains_all_fields))
+    print(get_valid_passports_count(passports, is_valid))
